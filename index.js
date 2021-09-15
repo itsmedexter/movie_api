6402e586301a4bb731dfe6f6effcf55c2a8e46cc
itsmedexter <deeside@pacbell.net>
2021-09-15 10:13:20 -0700

    fetching api data using axios, async and await

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+//  fetching api data using axios, async and await
+
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '3dd6a7a4',
+      s: 'avengers',
+    },
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
